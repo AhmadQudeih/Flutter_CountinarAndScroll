a0021e0b23b52cbd7f5139de4c375370598fdf53
Ahmad Qudeih <ahmadbfqudeih@gmail.com>
2023-09-11 20:17:34 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,180 +1,189 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          'facebook',
-          style: TextStyle(
-            color: Colors.blue,
-            fontWeight: FontWeight.bold,
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            'facebook',
+            style: TextStyle(
+              color: Colors.blue,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.more_horiz,
-            color: Colors.blue,
-          ),
-        ),
-        actions: [
-          IconButton(
+          centerTitle: true,
+          leading: IconButton(
             onPressed: () {},
             icon: Icon(
-              Icons.message,
+              Icons.more_horiz,
               color: Colors.blue,
             ),
           ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.search,
-              color: Colors.blue,
-            ),
-          ),
-        ],
-        elevation: 10,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            Container(
-              child: Text(
-                'ahmad',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 440,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.message,
+                color: Colors.blue,
               ),
             ),
-            SizedBox(height: 50),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: [
-                  Container(
-                    child: Text(
-                      'ahmad',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    alignment: Alignment.center,
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  SizedBox(width: 50),
-                  Container(
-                    child: Text(
-                      'ahmad',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    alignment: Alignment.center,
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  SizedBox(width: 50),
-                  Container(
-                    child: Text(
-                      'ahmad',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    alignment: Alignment.center,
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  SizedBox(width: 50),
-                  Container(
-                    child: Text(
-                      'ahmad',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    alignment: Alignment.center,
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  SizedBox(width: 50),
-                  Container(
-                    child: Text(
-                      'ahmad',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                      ),
-                    ),
-                    alignment: Alignment.center,
-                    width: 150,
-                    height: 80,
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: 50),
-            Container(
-              child: Text(
-                'ahmad',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-              ),
-              alignment: Alignment.center,
-              width: double.infinity,
-              height: 440,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.blue,
               ),
             ),
           ],
+          elevation: 10,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              Container(
+                child: Text(
+                  'ahmad',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: 440,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              SizedBox(height: 50),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      child: Text(
+                        'ahmad',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      width: 150,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    Container(
+                      child: Text(
+                        'ahmad',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      width: 150,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    Container(
+                      child: Text(
+                        'ahmad',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      width: 150,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    Container(
+                      child: Text(
+                        'ahmad',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      width: 150,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    SizedBox(width: 50),
+                    Container(
+                      child: Text(
+                        'ahmad',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                      alignment: Alignment.center,
+                      width: 150,
+                      height: 80,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 50),
+              Container(
+                child: Text(
+                  'ahmad',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: 440,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
